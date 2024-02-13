55b3a1e06f8d354967cc95697ee0860b524d5805
casava840 <hen3403@gmail.com>
2024-02-13 13:48:40 +0900

    update dynamic Consumer, Provider

--- src/contexts/color.js
+++ src/contexts/color.js
@@ -1,5 +1,30 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
-const ColorContext = createContext({ color: 'black' });
+const ColorContext = createContext({
+    state: {color: 'black', subcolor: 'red'},
+    action: {
+        setColor: () => {},
+        setSubcolor: () => {}
+    }
+});
 
+const ColorProvider = ({ children }) => {
+    const [color, setColor] = useState('black');
+    const [subcolor,setSubcolor] = useState('red');
+
+    const value = {
+        state: { color, subcolor},
+        action: {setColor, setSubcolor}
+    }
+
+    return(
+        <ColorContext.Provider value={value}>{children}</ColorContext.Provider>
+    );
+};
+
+// 아랫줄은 const ColorConsumer = ColorContext.Consumer와 같은 의미임
+const { Consumer: ColorConsumer } = ColorContext;
+
+//provider consumer 내보내기
+export { ColorProvider, ColorConsumer };
 export default ColorContext;
